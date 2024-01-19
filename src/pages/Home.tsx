abaca60a0a12838b2430ea5499c99d0132e14468
svanbrugh <svanbrugh@outlook.com>
2024-01-19 20:50:20 +1000

    Added Home Page Elements and Fixed Fonts/Bg.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,18 @@ import "./Home.scss";
 export const Home = () => {
     return (
         <div id="home-container">
-            <div id="home-text">Wedding Website Coming Soon!</div>
-            <img id="home-photo" src={photo} alt="homephoto" />
+            <div id="home-banner">Wedding Website Coming Soon!</div>
+
+            <div id="home-texts">
+                <h1 id="home-heading">Please Join Us September 2025</h1>
+                <p className="home-text">Details to come </p>
+                <p className="home-text"> </p>
+                <p className="home-text"></p>
+            </div>
+
+            <div id="homediv">
+                <img id="home-photo" src={photo} alt="homephoto" />
+            </div>
         </div>
     );
 };
