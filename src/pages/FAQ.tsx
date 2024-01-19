1fb2dd59ae9069c77d7bd33b682bdb8d2e6fe45a
svanbrugh <svanbrugh@outlook.com>
2024-01-19 21:58:49 +1000

    Adding FAQ elements and questions.

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,3 +1,53 @@
+import "./FAQ.scss";
 export const FAQ = () => {
-    return <div>Hello FAQ!</div>;
+    return (
+        <div id="faq-texts">
+            <h2 className="question-heading">Q: Can I Bring A Friend?</h2>
+            <p className="faq-text">
+                A: Unfortunately, due to budget and space limitations, we simply cannot afford for all of our lovely
+                guests to bring a guest of their own. Therefore, we regretfully advise our guests to please not bring a
+                plus one, unless they are specifically named on the invitation. Thank you so much for understanding!
+            </p>
+            <h2 className="question-heading">Q: Are Kids Welcome?</h2>
+            <p className="faq-text">
+                A: We are hoping for an intimate wedding with all our close family and friends, therefore unless
+                specifically mentioned on the invitation we are unable to accomofate for any extra kids/other family
+                members. If there is a specific reason for these individuals to attend please let Kien Vi or Sam know
+                and we will happily revise the guest list accordingly!
+            </p>
+            <h2 className="question-heading">Q: I Have Dietary Restrictions, Is That Okay?</h2>
+            <p className="faq-text">
+                A: Of course! We will happily cater for any restrictions that is required. Please help us by letting us
+                know when you RVSP by selecting an option from the drop down menu and including how many guests require
+                these special meals.
+            </p>
+            <h2 className="question-heading">Q: What Attire Should We Wear?</h2>
+            <p className="faq-text">
+                A: We would love for everyone to dress in Semi-Formal/Formal attire. Florals/bright colours are
+                definitely encouraged!
+            </p>
+            <h2 className="question-heading">Q: Is There Parking Available?</h2>
+            <p className="faq-text">
+                A: This is to be confirmed by our wedding location, so please keep an eye on this space for parking
+                updates!
+            </p>
+            <h2 className="question-heading">Q: Is There A Wedding Registry?</h2>
+            <p className="faq-text">
+                A: The most important thing to us is that you are able to celebrate with us on our wedding day. However,
+                if you wish to honour us with a gift, a cash gift would be very welcome.
+            </p>
+            <h2 className="question-heading">Q: Are There Any COVID Restrictions?</h2>
+            <p className="faq-text">
+                A: At this current point in time there are no such restrictions and anyone is welcome at our wedding! If
+                you wish to wear a mask, we are more than happy to supply extra as needed!
+            </p>
+            <h2 className="question-heading">Q: When Should We RVSP By?</h2>
+            <p className="faq-text">
+                A: We would love for everyone to RVSP as soon as possible to ensure that we can cater to everyones needs
+                promptly! We are asking for RVSPs to be in before the 1st of August 2025.
+            </p>
+            <h2 className="question-heading">Q: Do You Have A Wedding Hashtag?</h2>
+            <p className="faq-text">A: #KienSamBecomeALam?</p>
+        </div>
+    );
 };
