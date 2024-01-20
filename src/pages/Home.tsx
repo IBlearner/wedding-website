3afb852a497f70f3d7f9f453ed2d7c124e9ff6bc
svanbrugh <svanbrugh@outlook.com>
2024-01-20 13:21:10 +1000

    Adding name banner to every page and extra styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
-import photo from "../assets/homepic.png";
+import photo from "../assets/homeviet.png";
 
 import "./Home.scss";
 export const Home = () => {
     return (
         <div id="home-container">
-            <div id="home-banner">Wedding Website Coming Soon!</div>
-
             <div id="home-texts">
                 <h1 id="home-heading">Please Join Us September 2025</h1>
-                <p className="home-text">Details to come </p>
+                <p className="home-text">
+                    We would love everyone to join us at ______ on ______ September 2025 at ___:___pm. Please see the
+                    FAQ page for any queries and RVSP by the ____ of August 2025 to ensure all dietary requirements are
+                    met. Thank you everyone! Hope to see you all there!
+                </p>
                 <p className="home-text"> </p>
                 <p className="home-text"></p>
             </div>
