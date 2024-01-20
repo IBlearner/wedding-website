4642eb6c765998fc2c5ebcf1c1d2808fd651958b
kienvi <kienvi96@hotmail.com>
2024-01-20 12:28:18 +1000

    Created basic functionality for RSVP dropdown search.

--- src/App.tsx
+++ src/App.tsx
@@ -7,69 +7,69 @@ import { RSVP } from "./pages/RSVP";
 import { Schedule } from "./pages/Schedule";
 
 function App() {
-    const [currentPage, setCurrentPage] = useState("Home");
+	const [currentPage, setCurrentPage] = useState("Home");
 
-    const getCurrentPage = () => {
-        if (currentPage === "Home") {
-            return <Home />;
-        } else if (currentPage === "OurStory") {
-            return <OurStory />;
-        } else if (currentPage === "Schedule") {
-            return <Schedule />;
-        } else if (currentPage === "FAQ") {
-            return <FAQ />;
-        } else if (currentPage === "RSVP") {
-            return <RSVP />;
-        }
-    };
+	const getCurrentPage = () => {
+		if (currentPage === "Home") {
+			return <Home />;
+		} else if (currentPage === "OurStory") {
+			return <OurStory />;
+		} else if (currentPage === "Schedule") {
+			return <Schedule />;
+		} else if (currentPage === "FAQ") {
+			return <FAQ />;
+		} else if (currentPage === "RSVP") {
+			return <RSVP />;
+		}
+	};
 
-    return (
-        <>
-            <div id="buttonGroup">
-                <button
-                    className={`barButtons ${currentPage === "Home" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("Home");
-                    }}
-                >
-                    Home
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "OurStory" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("OurStory");
-                    }}
-                >
-                    Our Story
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "Schedule" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("Schedule");
-                    }}
-                >
-                    Schedule
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "FAQ" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("FAQ");
-                    }}
-                >
-                    FAQ
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "RSVP" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("RSVP");
-                    }}
-                >
-                    RSVP
-                </button>
-            </div>
-            {getCurrentPage()}
-        </>
-    );
+	return (
+		<>
+			<div id="buttonGroup">
+				<button
+					className={`barButtons ${currentPage === "Home" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("Home");
+					}}
+				>
+					Home
+				</button>
+				<button
+					className={`barButtons ${currentPage === "OurStory" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("OurStory");
+					}}
+				>
+					Our Story
+				</button>
+				<button
+					className={`barButtons ${currentPage === "Schedule" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("Schedule");
+					}}
+				>
+					Schedule
+				</button>
+				<button
+					className={`barButtons ${currentPage === "FAQ" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("FAQ");
+					}}
+				>
+					FAQ
+				</button>
+				<button
+					className={`barButtons ${currentPage === "RSVP" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("RSVP");
+					}}
+				>
+					RSVP
+				</button>
+			</div>
+			{getCurrentPage()}
+		</>
+	);
 }
 
 export default App;
--- src/pages/RSVP.tsx
+++ src/pages/RSVP.tsx
@@ -1,3 +1,123 @@
+import { useEffect, useState } from "react";
+import { google } from "googleapis";
+import "./RSVP.scss";
+
 export const RSVP = () => {
-    return <div>Hello RSVP!</div>;
+	const [namesList, setNamesList] = useState([]);
+	const [filteredNamesList, setfilteredNamesList] = useState([]);
+	const [nameSearch, setNameSearch] = useState("");
+
+	useEffect(() => {
+		// const auth = new google.auth.GoogleAuth({
+		// 	keyFile: "../../single-portal-410911-df162cabc822.json",
+		// 	scopes: ["https://www.googleapis.com/auth/sheets"]
+		// });
+
+		getGuestList();
+	}, []);
+
+	useEffect(() => {
+		const arr = namesList.filter((elem: string) => {
+			return elem.includes(nameSearch);
+		});
+		setfilteredNamesList(arr);
+	}, [nameSearch]);
+
+	const getGuestList = () => {
+		fetch(
+			"https://sheets.googleapis.com/v4/spreadsheets/1AbMVXsGdPteKIpDI8OD3Va5FhlKGtNLfPcQiskoHvZU/values/Wedding guests!A1:B61?key=AIzaSyDXlaLu9_omOiZtqcwVbH-c0uVwRg9lR7E",
+			{
+				method: "GET"
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((res) => {
+				setNamesList(
+					res.values.map((elem: string) => {
+						return elem[0].toLowerCase();
+					})
+				);
+
+				let sortedData = {};
+				res.values
+					.map((elem: string[]) => {
+						return elem;
+					})
+					.forEach((elem: string[]) => {
+						if (sortedData.hasOwnProperty(elem[1])) {
+							sortedData[elem[1]] = [...sortedData[elem[1]], elem[0]];
+						} else {
+							sortedData[elem[1]] = [elem[0]];
+						}
+					});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		console.log("submitting form..");
+	};
+
+	const submitForm = () => {
+		fetch(
+			"https://sheets.googleapis.com/v4/spreadsheets/1AbMVXsGdPteKIpDI8OD3Va5FhlKGtNLfPcQiskoHvZU/values/Wedding guests!A1:B2?key=AIzaSyDXlaLu9_omOiZtqcwVbH-c0uVwRg9lR7E",
+			{
+				method: "PUT",
+				body: {
+					range: "Wedding guests!C1:C2",
+					values: [["fdsfs"], ["fsnds"]]
+				}
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		console.log("submitting form..");
+	};
+
+	const onNameSearchChange = (val) => {
+		setNameSearch(val.target.value);
+		const regex = "";
+	};
+
+	const getNameSearch = () => {
+		return (
+			<div id="name-search">
+				{!nameSearch ? null : <label htmlFor="name-search">Search for your name</label>}
+				<input
+					name="name-search"
+					type="text"
+					placeholder="Search your name!"
+					onChange={(val) => onNameSearchChange(val)}
+				/>
+				{!nameSearch ? null : getNameSearchDropdown()}
+			</div>
+		);
+	};
+
+	const getNameSearchDropdown = () => {
+		return filteredNamesList.map((elem) => {
+			return (
+				<div className="name-search-dropdown-option" tabIndex={0}>
+					{elem}
+				</div>
+			);
+		});
+	};
+
+	return (
+		<>
+			<button onClick={submitForm}>clickk</button>
+			<form onSubmit={submitForm}>
+				{getNameSearch()}
+				<label htmlFor=""></label>
+				{/* Name: <input type="text" name="fname" /> */}
+				{/* <input type="submit" value="Submit" /> */}
+			</form>
+		</>
+	);
 };
