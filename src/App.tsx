495f64bbe188f98e367a44892fba025d5db0a79f
kienvi <kienvi96@hotmail.com>
2024-01-29 21:23:42 +1000

    Adding hamburger to toggle menu tabs in mobile mode + mobile mode styling.

--- src/App.tsx
+++ src/App.tsx
@@ -7,75 +7,100 @@ import { RSVP } from "./pages/RSVP";
 import { Schedule } from "./pages/Schedule";
 
 function App() {
-    const [currentPage, setCurrentPage] = useState("Home");
+	const [currentPage, setCurrentPage] = useState<string>("Home");
+	const [mobileMenuVisible, setMobileMenuVisible] = useState<boolean>(true);
 
-    const getCurrentPage = () => {
-        if (currentPage === "Home") {
-            return <Home />;
-        } else if (currentPage === "OurStory") {
-            return <OurStory />;
-        } else if (currentPage === "Schedule") {
-            return <Schedule />;
-        } else if (currentPage === "FAQ") {
-            return <FAQ />;
-        } else if (currentPage === "RSVP") {
-            return <RSVP />;
-        }
-    };
+	const getCurrentPage = () => {
+		if (currentPage === "Home") {
+			return <Home />;
+		} else if (currentPage === "OurStory") {
+			return <OurStory />;
+		} else if (currentPage === "Schedule") {
+			return <Schedule />;
+		} else if (currentPage === "FAQ") {
+			return <FAQ />;
+		} else if (currentPage === "RSVP") {
+			return <RSVP />;
+		}
+	};
 
-    return (
-        <>
-            <div id="home-banner">
-                <h1 className="home-name">╼ Kien Vi </h1>
-                <h2 id="home-other">&</h2>
-                <h1 className="home-name">Samantha ╾ </h1>
-            </div>
-            <div id="buttonGroup">
-                <button
-                    className={`barButtons ${currentPage === "Home" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("Home");
-                    }}
-                >
-                    ❀ Home ❀
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "OurStory" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("OurStory");
-                    }}
-                >
-                    ❀ Our Story ❀
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "Schedule" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("Schedule");
-                    }}
-                >
-                    ❀ Schedule ❀
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "FAQ" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("FAQ");
-                    }}
-                >
-                    ❀ FAQ ❀
-                </button>
-                <button
-                    className={`barButtons ${currentPage === "RSVP" ? "selected" : ""}`}
-                    onClick={() => {
-                        setCurrentPage("RSVP");
-                    }}
-                >
-                    ❀ RSVP ❀
-                </button>
-            </div>
+	const checkDeviceIsMobile = () => {
+		return window.innerWidth <= 768;
+	};
 
-            {getCurrentPage()}
-        </>
-    );
+	const getMobileMenuButton = () => {
+		return (
+			<button id="mobile-menu-button" onClick={() => setMobileMenuVisible(!mobileMenuVisible)}>
+				~
+			</button>
+		);
+	};
+
+	const closeMobileMenu = () => {
+		setMobileMenuVisible(false);
+	};
+
+	return (
+		<>
+			<div id="home-banner">
+				<h1 className="home-name">╼ Kien Vi </h1>
+				<h2 id="home-other">&</h2>
+				<h1 className="home-name">Samantha ╾ </h1>
+				{checkDeviceIsMobile() ? getMobileMenuButton() : null}
+			</div>
+			{mobileMenuVisible ? (
+				<div id="buttonGroup">
+					<button
+						className={`barButtons ${currentPage === "Home" ? "selected" : ""}`}
+						onClick={() => {
+							setCurrentPage("Home");
+							closeMobileMenu();
+						}}
+					>
+						❀ Home ❀
+					</button>
+					<button
+						className={`barButtons ${currentPage === "OurStory" ? "selected" : ""}`}
+						onClick={() => {
+							setCurrentPage("OurStory");
+							closeMobileMenu();
+						}}
+					>
+						❀ Our Story ❀
+					</button>
+					<button
+						className={`barButtons ${currentPage === "Schedule" ? "selected" : ""}`}
+						onClick={() => {
+							setCurrentPage("Schedule");
+							closeMobileMenu();
+						}}
+					>
+						❀ Schedule ❀
+					</button>
+					<button
+						className={`barButtons ${currentPage === "FAQ" ? "selected" : ""}`}
+						onClick={() => {
+							setCurrentPage("FAQ");
+							closeMobileMenu();
+						}}
+					>
+						❀ FAQ ❀
+					</button>
+					<button
+						className={`barButtons ${currentPage === "RSVP" ? "selected" : ""}`}
+						onClick={() => {
+							setCurrentPage("RSVP");
+							closeMobileMenu();
+						}}
+					>
+						❀ RSVP ❀
+					</button>
+				</div>
+			) : null}
+
+			{getCurrentPage()}
+		</>
+	);
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,20 @@ import photo from "../assets/homeviet.png";
 
 import "./Home.scss";
 export const Home = () => {
-    return (
-        <div id="home-container">
-            <div id="home-texts">
-                <h1 id="home-heading">Please Join Us September 2025</h1>
-                <p className="home-text">
-                    We would love everyone to join us at ______ on ______ September 2025 at ___:___pm. Please see the
-                    FAQ page for any queries and RVSP by the ____ of August 2025 to ensure all dietary requirements are
-                    met. Thank you everyone! Hope to see you all there!
-                </p>
-                <p className="home-text"> </p>
-                <p className="home-text"></p>
-            </div>
+	return (
+		<div id="home-container">
+			<div id="home-texts">
+				<h1 id="home-heading">Please Join Us September 2025</h1>
+				<p className="home-text">
+					We would love everyone to join us at ______ on ______ September 2025 at ___:___pm. Please see the
+					FAQ page for any queries and RVSP by the ____ of August 2025 to ensure all dietary requirements are
+					met. Thank you everyone! Hope to see you all there!
+				</p>
+			</div>
 
-            <div id="homediv">
-                <img id="home-photo" src={photo} alt="homephoto" />
-            </div>
-        </div>
-    );
+			<div id="homediv">
+				<img id="home-photo" src={photo} alt="homephoto" />
+			</div>
+		</div>
+	);
 };
