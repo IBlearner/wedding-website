df73abf1becad01f4b2f4d9461671d0915f62e32
kienvi <kienvi96@hotmail.com>
2024-08-11 16:31:43 +1000

    Creating basic footer to be displayed on all pages.

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+
+export const Footer = () => {
+	return (
+		<div id="footer-container">
+			<span>Kienvi Lam</span>
+			<span>and</span>
+			<span>Samantha Vanbrugh</span>
+		</div>
+	);
+};
