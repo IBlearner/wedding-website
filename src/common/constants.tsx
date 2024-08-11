fe1a4229c3cd9eb0a2a421a9d1db10428a75a403
kienvi <kienvi96@hotmail.com>
2024-08-11 16:34:48 +1000

    Updating faq to be a list that can be expanded.

---
+++ src/common/constants.tsx
@@ -0,0 +1,49 @@
+import { Ifaq } from "./types";
+
+export const faqData: Ifaq[] = [
+	{
+		id: 1,
+		question: "Can I bring a friend?",
+		answer: "Unfortunately, due to budget and space limitations, we simply cannot afford for all of our lovely guests to bring a guest of their own. Therefore, we regretfully advise our guests to please not bring a plus one, unless they are specifically named on the invitation. Thank you so much for understanding!"
+	},
+	{
+		id: 2,
+		question: "Are kids welcome?",
+		answer: "We are hoping for an intimate wedding with all our close family and friends, therefore unless specifically mentioned on the invitation we are unable to accomofate for any extra kids/other family members. If there is a specific reason for these individuals to attend please let Kien Vi or Sam knowand we will happily revise the guest list accordingly!"
+	},
+	{
+		id: 3,
+		question: "What if I have dietary restrictions?",
+		answer: "No worries! We will happily cater for any restrictions that is required. Please help us by letting us know when you RVSP by selecting an option from the drop down menu and including how many guests require these special meals."
+	},
+	{
+		id: 4,
+		question: "What attire to wear?",
+		answer: "We would love for everyone to dress in Semi-Formal/Formal attire. Florals/bright colours are definitely encouraged!"
+	},
+	{
+		id: 5,
+		question: "Is there parking?",
+		answer: "We are hoping for an intimate wedding with all our close family and friends, therefore unless specifically mentioned on the invitation we are unable to accomofate for any extra kids/other family members. If there is a specific reason for these individuals to attend please let Kien Vi or Sam knowand we will happily revise the guest list accordingly!This is to be confirmed by our wedding location, so please keep an eye on this space for parking	updates!"
+	},
+	{
+		id: 6,
+		question: "Is there a registry?",
+		answer: "The most important thing to us is that you are able to celebrate with us on our wedding day. However, if you wish to honour us with a gift, a cash gift would be very welcome."
+	},
+	{
+		id: 7,
+		question: "Are COVID restrictions?",
+		answer: "At this current point in time there are no such restrictions and anyone is welcome at our wedding! If you wish to wear a mask, we are more than happy to supply extra as needed!"
+	},
+	{
+		id: 8,
+		question: "When should I rsvp by?",
+		answer: "We would love for everyone to RVSP as soon as possible to ensure that we can cater to everyones needs promptly! We are asking for RVSPs to be in before the 1st of August 2025."
+	},
+	{
+		id: 9,
+		question: "Do you have a wedding hashtag?",
+		answer: "#KienSamBecomeALam?"
+	}
+];
---
+++ src/common/types.tsx
@@ -0,0 +1,5 @@
+export interface Ifaq {
+	id: number;
+	question: string;
+	answer: string;
+}
