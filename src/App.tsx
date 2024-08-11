e54759f2a88322233a0c4b2eb9cab2eec6cde1e2
kienvi <kienvi96@hotmail.com>
2024-08-11 16:34:07 +1000

    Updating App and Home to be simplified.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./App.scss";
 import { Home } from "./pages/Home";
 import { OurStory } from "./pages/OurStory";
-import { FAQ } from "./pages/FAQ";
-import { RSVP } from "./pages/RSVP";
-import { Schedule } from "./pages/Schedule";
+import { FAQ } from "./pages/faq";
+import { RSVP } from "./pages/rsvp";
+import { Schedule } from "./pages/schedule";
+import { Footer } from "./components/footer/footer";
+import { formatDistanceStrict } from "date-fns";
+// Icon imports
+import { IoIosMenu } from "react-icons/io";
 
 function App() {
 	const [currentPage, setCurrentPage] = useState<string>("Home");
-	const [mobileMenuVisible, setMobileMenuVisible] = useState<boolean>(true);
+	const [menuVisible, setMenuVisible] = useState<boolean>(false);
+	const weddingDay = new Date(2026, 3, 11);
+
+	useEffect(() => {
+		document.getElementById("mobile-menu-button")?.addEventListener("click", () => {
+			document.getElementById("mobile-menu-button")!.style.pointerEvents = "none";
+			setTimeout(() => {
+				document.getElementById("mobile-menu-button")!.style.pointerEvents = "auto";
+			}, 1000);
+		});
+	}, []);
 
 	const getCurrentPage = () => {
 		if (currentPage === "Home") {
@@ -30,75 +44,81 @@ function App() {
 
 	const getMobileMenuButton = () => {
 		return (
-			<button id="mobile-menu-button" onClick={() => setMobileMenuVisible(!mobileMenuVisible)}>
-				~
-			</button>
+			<div id="mobile-menu-button" onClick={() => setMenuVisible(!menuVisible)}>
+				<IoIosMenu size={24} />
+			</div>
 		);
 	};
 
 	const closeMobileMenu = () => {
-		setMobileMenuVisible(false);
+		setMenuVisible(false);
+	};
+
+	const getHomeBanner = () => {
+		return (
+			<div id="home-banner">
+				<h1>Kienvi & Samantha</h1>
+				<div>11th April 2026 • Brisbane Australia</div>
+				<div>{formatDistanceStrict(Date.now(), weddingDay, { unit: "day" })} more days to go!</div>
+			</div>
+		);
 	};
 
 	return (
 		<>
-			<div id="home-banner">
-				<h1 className="home-name">╼ Kien Vi </h1>
-				<h2 id="home-other">&</h2>
-				<h1 className="home-name">Samantha ╾ </h1>
-				{checkDeviceIsMobile() ? getMobileMenuButton() : null}
+			{getHomeBanner()}
+			{checkDeviceIsMobile() ? getMobileMenuButton() : null}
+
+			<div id="menu-tabs" className={`${menuVisible ? "menu-visible" : "menu-hidden"}`}>
+				<button
+					className={`menu-tab ${currentPage === "Home" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("Home");
+						closeMobileMenu();
+					}}
+				>
+					HOME
+				</button>
+				<button
+					className={`menu-tab ${currentPage === "OurStory" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("OurStory");
+						closeMobileMenu();
+					}}
+				>
+					OUR STORY
+				</button>
+				<button
+					className={`menu-tab ${currentPage === "Schedule" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("Schedule");
+						closeMobileMenu();
+					}}
+				>
+					SCHEDULE
+				</button>
+				<button
+					className={`menu-tab ${currentPage === "FAQ" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("FAQ");
+						closeMobileMenu();
+					}}
+				>
+					FAQ
+				</button>
+				<button
+					className={`menu-tab ${currentPage === "RSVP" ? "selected" : ""}`}
+					onClick={() => {
+						setCurrentPage("RSVP");
+						closeMobileMenu();
+					}}
+				>
+					RSVP
+				</button>
 			</div>
-			{mobileMenuVisible ? (
-				<div id="buttonGroup">
-					<button
-						className={`barButtons ${currentPage === "Home" ? "selected" : ""}`}
-						onClick={() => {
-							setCurrentPage("Home");
-							closeMobileMenu();
-						}}
-					>
-						❀ Home ❀
-					</button>
-					<button
-						className={`barButtons ${currentPage === "OurStory" ? "selected" : ""}`}
-						onClick={() => {
-							setCurrentPage("OurStory");
-							closeMobileMenu();
-						}}
-					>
-						❀ Our Story ❀
-					</button>
-					<button
-						className={`barButtons ${currentPage === "Schedule" ? "selected" : ""}`}
-						onClick={() => {
-							setCurrentPage("Schedule");
-							closeMobileMenu();
-						}}
-					>
-						❀ Schedule ❀
-					</button>
-					<button
-						className={`barButtons ${currentPage === "FAQ" ? "selected" : ""}`}
-						onClick={() => {
-							setCurrentPage("FAQ");
-							closeMobileMenu();
-						}}
-					>
-						❀ FAQ ❀
-					</button>
-					<button
-						className={`barButtons ${currentPage === "RSVP" ? "selected" : ""}`}
-						onClick={() => {
-							setCurrentPage("RSVP");
-							closeMobileMenu();
-						}}
-					>
-						❀ RSVP ❀
-					</button>
-				</div>
-			) : null}
 
 			{getCurrentPage()}
+			{<Footer />}
 		</>
 	);
 }
